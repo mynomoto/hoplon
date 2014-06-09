527f59e87e213b9dfac5930f3b17a63f4565c417
Micha Niskin <micha.niskin@gmail.com>
2014-06-09 03:42:47 -0400

    workin

--- src/tailrecursion/hoplon.cljs
+++ src/tailrecursion/hoplon.cljs
@@ -15,7 +15,7 @@
    [cljs.reader           :refer [read-string]]
    [clojure.string        :refer [split join blank?]]))
 
-(declare do! on! $text add-children!)
+(declare do! on! $text span add-children!)
 
 (def is-ie8 (not (aget js/window "Node")))
 
@@ -46,6 +46,10 @@
   ([f] (timeout f 0))
   ([f t] (.setTimeout js/window f t)))
 
+(defn jqdata
+  ([this k] (.data (js/jQuery this) (str k)))
+  ([this k v] (.data (js/jQuery this) (str k) v) this))
+
 ;; env ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn unsplice [forms]
@@ -59,47 +63,84 @@
         (if (.contains (.-documentElement js/document) this) (f) (timeout doit 20))))))
 
 (defn parse-args [[head & tail :as args]]
-  (let [kw1? (comp keyword? first)
-        mkkw #(->> (partition 2 %) (take-while kw1?) (map vec))
-        drkw #(->> (partition 2 2 [] %) (drop-while kw1?) (mapcat identity))]
+  (let [kw1?  (comp keyword? first)
+        mapc? #(and (cell? %) (map? @%))
+        mkkw  #(->> (partition 2 %) (take-while kw1?) (map vec))
+        drkw  #(->> (partition 2 2 [] %) (drop-while kw1?) (mapcat identity))]
     (cond
       (map?     head) [head (unsplice tail)]
+      (mapc?    head) [{:attr head} (unsplice tail)]
       (keyword? head) [(into {} (mkkw args)) (unsplice (drkw args))]
       :else           [{} (unsplice args)])))
 
 (defn add-attributes! [this attr]
-  (let [key*   #(let [n (let [s (name %2), c (last s)]
-                          (if-not (= \= c) s (.slice s 0 -1)))
-                      p (.substr n 0 3)] 
-                  (keyword (namespace %2) (if-not (= %1 p) n (.substr n 3))))
-        dokey  (partial key* "do-")
-        onkey  (partial key* "on-")
-        dos    (atom {}) 
-        ons    (atom {})
-        addcls #(join " " (-> %1 (split #" ") set (into (split %2 #" "))))]
-    (doseq [[k v] attr]
-      (cond
-        (cell? v) (swap! dos assoc (dokey k) v)
-        (fn? v)   (swap! ons assoc (onkey k) v)
-        :else     (do! this (dokey k) v)))
-    (when (seq @dos)
-      (with-timeout 0
-        (doseq [[k v] @dos]
-          (do! this k @v)
-          (add-watch v (gensym) #(do! this k %4)))))
-    (when (seq @ons)
-      (with-timeout 0
-        (doseq [[k v] @ons]
-          (on! this k v)))) 
-    this))
+  (if-let [tpl (aget this "_hoplon_attr")]
+    (do (swap! tpl merge attr) this)
+    (let [key*   #(let [n (let [s (name %2), c (last s)]
+                            (if-not (= \= c) s (.slice s 0 -1)))
+                        p (.substr n 0 3)] 
+                    (keyword (namespace %2) (if-not (= %1 p) n (.substr n 3))))
+          dokey  (partial key* "do-")
+          onkey  (partial key* "on-")
+          dos    (atom {}) 
+          ons    (atom {})
+          addcls #(join " " (-> %1 (split #" ") set (into (split %2 #" "))))]
+      (doseq [[k v] attr]
+        (cond
+          (cell? v) (swap! dos assoc (dokey k) v)
+          (fn? v)   (swap! ons assoc (onkey k) v)
+          :else     (do! this (dokey k) v)))
+      (when (seq @dos)
+        (with-timeout 0
+          (doseq [[k v] @dos]
+            (do! this k @v)
+            (add-watch v (gensym) #(do! this k %4)))))
+      (when (seq @ons)
+        (with-timeout 0
+          (doseq [[k v] @ons]
+            (on! this k v)))) 
+      this)))
 
 (def append-child
   (if-not is-ie8
     #(.appendChild %1 %2)
     #(try (.appendChild %1 %2) (catch js/Error _))))
 
+(def remove-child
+  (if-not is-ie8
+    #(.removeChild %1 %2)
+    #(try (.removeChild %1 %2) (catch js/Error _))))
+
+(def replace-child
+  (if-not is-ie8
+    #(.replaceChild %1 %2 %3)
+    #(try (.replaceChild %1 %2 %3) (catch js/Error _))))
+
+(def insert-before
+  (if-not is-ie8
+    #(.insertBefore %1 %2 %3)
+    #(try (.insertBefore %1 %2 %3) (catch js/Error _))))
+
+(defn parent-node [this]
+  (or (jqdata this :hoplon-parent) (.-parentNode this)))
+
+(defn first-child [this]
+  (if-let [kids (aget this "_hoplon_children")]
+    (first (.call kids))
+    (.-firstChild this)))
+
+(defn get-children [this]
+  (if-let [kids (aget this "_hoplon_children")]
+    (.call kids)
+    (.. (js/jQuery this) children get)))
+
+(defn remove-children [this]
+  (if-let [empty (aget this "_hoplon_empty")]
+    (.call empty)
+    (.. (js/jQuery this) empty)))
+
 (defn replace-children! [this new-children]
-  (.empty (js/jQuery this))
+  (remove-children this)
   (add-children! this (if (sequential? new-children) new-children [new-children])))
 
 (defn add-children! [this [child-cell & _ :as kids]]
@@ -110,7 +151,42 @@
       (doseq [x (keep node (unsplice kids))] (append-child this x))))
   this)
 
-(defn on-append! [this f]
+(defn tpl* [tpl]
+  (fn [& args]
+    (let [attr     (cell {})
+          children (cell [])
+          this     (tpl attr children)
+          setp     #(doto % (jqdata :hoplon-parent this))]
+      (doto this
+        (aset "_hoplon_attr"     attr)
+        (aset "_hoplon_children" #(deref children))
+        (aset "_hoplon_empty"    #(swap! children empty))
+        (aset "appendChild"      #(swap! children concat [(setp %)]))
+        (aset "removeChild"      #(swap! children (partial filter (partial not= %))))
+        (aset "replaceChild"     #(swap! children (partial map (fn [x] (if (= x %2) (setp %1) x)))))
+        (aset "insertBefore"     #(swap! children (partial mapcat (fn [x] (if (= x %2) [(setp %1) x] [x])))))
+        (apply args)))))
+
+(defn loop-tpl*
+  [items reverse? tpl]
+  (let [pool-size  (cell  0)
+        items-seq  (cell= (seq items))
+        cur-count  (cell= (count items-seq))
+        show-ith?  #(cell= (< % cur-count))
+        ith-item   #(cell= (safe-nth items-seq %))]
+    (with-let [d (span)]
+      (when-dom d
+        #(let [p (parent-node d)]
+           (remove-child p d)
+           (cell= (when (< pool-size cur-count)
+                    (doseq [i (range pool-size cur-count)]
+                      (let [e ((tpl (ith-item i)) ::toggle (show-ith? i))]
+                        (if-not reverse?
+                          (append-child p e)
+                          (insert-before p e (first-child p)))))
+                    (reset! ~(cell pool-size) cur-count))))))))
+
+(defn ^:deprecated on-append! [this f]
   (set! (.-hoplonIFn this) f))
 
 (extend-type js/Element
@@ -323,10 +399,12 @@
   [elem _ kvs]
   (let [e (js/jQuery elem)]
     (doseq [[k v] kvs]
-      (let [k (name k)]
-        (if (= false v)
-          (.removeAttr e k)
-          (.attr e k (if (= true v) k v)))))))
+      (if (cell? v)
+        (add-attributes! elem {k v})
+        (let [k (name k)]
+          (if (= false v)
+            (.removeAttr e k)
+            (.attr e k (if (= true v) k v))))))))
 
 (defmethod do! :class
   [elem _ kvs] 
@@ -386,30 +464,20 @@
           elem (js/jQuery elem)]
       (.animate body (clj->js {:scrollTop (.-top (.offset elem))})))))
 
+(defmethod do! ::toggle
+  [elem _ v]
+  (if-let [p (jqdata elem :hoplon-parent)]
+    (let [rm #(apply jqdata elem :hoplon-removed %&)
+          rc #(do (rm true) (remove-child p elem))
+          ac #(when (rm) (rm false) (append-child p elem))]
+      ((if v ac rc) p elem))
+    (do! elem :toggle v)))
+
 (defmulti on! (fn [elem event callback] event) :default ::default)
 
 (defmethod on! ::default
   [elem event callback]
   (when-dom elem #(.on (js/jQuery elem) (name event) callback)))
-
-(defn loop-tpl*
-  [items reverse? tpl]
-  (let [pool-size  (cell  0)
-        items-seq  (cell= (seq items))
-        cur-count  (cell= (count items-seq))
-        show-ith?  #(cell= (< % cur-count))
-        ith-item   #(cell= (safe-nth items-seq %))]
-    (with-let [d (span)]
-      (when-dom d
-        #(let [p (.-parentNode d)]
-           (.removeChild p d)
-           (cell= (when (< pool-size cur-count)
-                    (doseq [i (range pool-size cur-count)]
-                      (let [e ((tpl (ith-item i)) :do-toggle (show-ith? i))]
-                        (if-not reverse?
-                          (.appendChild p e)
-                          (.insertBefore p e (.-firstChild p)))))
-                    (reset! ~(cell pool-size) cur-count))))))))
 
 (defn route-cell
   "Manage the URL hash via Javelin cells. There are three arities:
