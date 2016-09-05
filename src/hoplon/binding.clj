21962867075429eee3b985fe6705d83563a74f93
Micha Niskin <micha.niskin@gmail.com>
2016-09-05 03:28:04 -0400

    Cleanup, make bound-fn more efficient

--- src/hoplon/binding.clj
+++ src/hoplon/binding.clj
@@ -17,19 +17,20 @@
   "Like clojure.core/binding, but can be used with bound-fn. The bindings
   should not be ^:dynamic -- they must be ^:export instead."
   [bindings & body]
-  (let [env  (assoc &env :ns (a/get-namespace a/*cljs-ns*))
-        sets (take-nth 2 (rest bindings))
-        vars (->> (take-nth 2 bindings)
-                  (map (partial a/resolve-existing-var env)))
-        syms (map :name vars)
-        gens (take (count vars) (repeatedly gensym))
-        jses (map (comp str c/munge) syms)
-        lexs (interleave gens sets)
-        bmap (zipmap jses gens)
-        bind (interleave syms gens)]
-    (doseq [v vars] (confirm-export env v))
-    `(let [~@lexs]
-       (hoplon.binding/push-thread-bindings ~bmap)
+  (let [env           (assoc &env :ns (a/get-namespace a/*cljs-ns*))
+        value-exprs   (take-nth 2 (rest bindings))
+        vars-to-bind  (->> (take-nth 2 bindings)
+                           (map (partial a/resolve-existing-var env)))
+        js-vars       (map c/munge (map :name vars-to-bind))
+        restore-syms  (take (count vars-to-bind) (repeatedly gensym))
+        set-syms      (take (count vars-to-bind) (repeatedly gensym))
+        setfn         (fn [x y] `(fn [] (set! ~x ~y)))
+        push-pop      (fn [x y z] {:push! (setfn x y) :pop! (setfn x z)})
+        thunkmaps     (map push-pop js-vars set-syms restore-syms)]
+    (doseq [v vars-to-bind] (confirm-export env v))
+    `(let [~@(interleave set-syms value-exprs)
+           ~@(interleave restore-syms js-vars)]
+       (hoplon.binding/push-thread-bindings ~(zipmap (map str js-vars) thunkmaps))
        (try ~@body (finally (hoplon.binding/pop-thread-bindings))))))
 
 (defmacro bound-fn
--- src/hoplon/binding.cljs
+++ src/hoplon/binding.cljs
@@ -2,9 +2,7 @@
   (:refer-clojure :exclude [binding])
   (:require-macros [hoplon.binding :as b]))
 
-(def ^:export tmp    "Temporary variable for eval." nil)
-(def thread-bindings "Stack of binding maps."       (atom []))
-(def global-bindings "Map of initial var bindings." (atom {}))
+(def thread-bindings "Stack of binding maps." (atom []))
 
 (defn push-thread-bindings
   "Given a map with munged js variable names (as strings) for keys and the
@@ -13,23 +11,16 @@
   bindings for a variable its current value is stored in the global-bindings
   map so it can be restored later."
   [binding-map]
-  (let [current (apply merge @thread-bindings)]
-    (swap! thread-bindings conj binding-map)
-    (doseq [[k v] binding-map]
-      (when-not (contains? current k)
-        (swap! global-bindings assoc k (js* "eval(~{})" k)))
-      (set! hoplon.binding.tmp v)
-      (js* "eval(~{})" (str k " = hoplon.binding.tmp" )))))
+  (swap! thread-bindings conj binding-map)
+  (doseq [{:keys [push!]} (vals binding-map)] (push!)))
 
 (defn pop-thread-bindings
   "Pops the topmost binding map from thread-bindings stack and restores the
   variables to their previous saved states."
   []
-  (let [popped  (peek @thread-bindings)
-        current (apply merge (swap! thread-bindings pop))]
-    (doseq [k (keys popped)]
-      (set! hoplon.binding.tmp (get current k (get @global-bindings k)))
-      (js* "eval(~{})" (str k " = hoplon.binding.tmp" )))))
+  (let [popped (peek @thread-bindings)]
+    (swap! thread-bindings pop)
+    (doseq [{:keys [pop!]} (vals popped)] (pop!))))
 
 (defn bound-fn*
   "Given a function f, returns a new function capturing the current bindings
